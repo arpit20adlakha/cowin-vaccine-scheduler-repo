5cbb0b687f5d6f1ecf91fa85831ea384604a0f7f
arpit.adlakha <arpit.adlakha@gojek.com>
2021-05-02 21:06:59 +0530

    cron twilio setup

--- index.js
+++ index.js
@@ -2,15 +2,19 @@
 
 const axios = require('axios');
 const prompt = require('prompt-sync')();
-// const cron = require(node-cron);
+const cron = require('node-cron');
+const message = require('./send-whatsapp');
+const fs = require('fs');
 
 async function main() {
+    let rawConfig = fs.readFileSync('./config.json');
+    let config = JSON.parse(rawConfig);
     let url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin";
-    let pinCode;
-    let dates = [];
-    pinCode = prompt("Enter your pin code ");
-    let dateString = prompt("Enter the dates in a comma seperated format '03-04-2021,04-04-2021' where the data format is DD-MM-YYYY ");
-    dates = dateString.split(',');
+    let pinCode = config.pincode;
+    let dates = config.dates;
+    // pinCode = prompt("Enter your pin code ");
+    // let dateString = prompt("Enter the dates in a comma separated format '03-04-2021,04-04-2021' where the data format is DD-MM-YYYY ");
+    // dates = dateString.split(',');
     let arrResponses = [];
 
     url = addPinCode(url, pinCode);
@@ -27,20 +31,40 @@ async function main() {
 
 async function callMain() {
     const answers = await main();
+    // if (Array.isArray(answers) && answers.length > 0 && answers[0].availability.sessions[0]) {
+    //     let date = answers[0].date;
+    //     let time = JSON.stringify(answers[0].availability.sessions[0].slots);
+    //     let hospital = answers[0].availability.sessions[0].name;
+    //     sendMessage.sendMessage(date, time, hospital);
+    // } else {
+    //     sendMessage(answers[0].date);
+    // }
     for(let obj of answers) {
         console.log("Appointments available for date ", obj.date);
         let sessions = obj.availability.sessions;
         for (let session of sessions) {
-             let slotString = session.slots;
-             session.slots = JSON.stringify(slotString);
+             let slotArray = session.slots;
+             let str = "slots available are :";
+             for(let i = 0; i < slotArray.length; i++) {
+                 str = str.concat(`\n${i + 1}. ${slotArray[i]}`);
+             }
+             session.slots = JSON.stringify(slotArray);
+
+             let {center_id, lat, long, session_id, ...formattedMsg} = session;
+
+             let answer = `For the date ${formattedMsg.date} the vaccine is available at Hospital ${formattedMsg.name} from ${formattedMsg.from} to ${formattedMsg.to}. Name of the vaccine available is ${formattedMsg.vaccine}. The capacity available is ${formattedMsg.available_capacity}. Fee charged for the vaccine is ${formattedMsg.fee}. And the ${str}`
+             message.sendMessageWhole(answer);
         }
         console.log(obj.availability);
     }
 }
 
-callMain().then(() => {
-    console.log("Get Vaccinated !!!");
+cron.schedule('*/1 * * * *', () => {
+    callMain().then(() => {
+        console.log("Get Vaccinated !!!");
+    });
 });
+
 
 function addPinCode(url, pincode) {
     return url.concat(`?pincode=${pincode}`);
---
+++ send-whatsapp.js
@@ -0,0 +1,43 @@
+const accountSid = 'ACca0fc81e7812240c2d1b09b53b911b59';
+const authToken = '3d06e2f49b8132166c059284afdfe758';
+const client = require('twilio')(accountSid, authToken);
+
+
+function sendMessage(date, time, hospital) {
+    if (hospital) {
+        client.messages
+            .create({
+                body: `The appointments  available at ${date} at ${time} hospital name is ${hospital}`,
+                from: 'whatsapp:+14155238886',
+                to: 'whatsapp:+919158043074'
+            })
+            .then(message => console.log(message.sid))
+            .done();
+    } else {
+        client.messages
+            .create({
+                body: `No appointments available on ${date}`,
+                from: 'whatsapp:+14155238886',
+                to: 'whatsapp:+919158043074'
+            })
+            .then(message => console.log(message.sid))
+            .done();
+    }
+}
+
+function sendMessageWhole(answer) {
+    client.messages
+        .create({
+            body: `${answer}`,
+            from: 'whatsapp:+14155238886',
+            to: 'whatsapp:+919158043074'
+        })
+        .then(message => console.log(message.sid))
+        .done();
+}
+
+
+module.exports = {
+    sendMessage : sendMessage,
+    sendMessageWhole: sendMessageWhole
+}

dc0642ffdeaac382fd5adbca8b9826a789d28143
arpit.adlakha <arpit.adlakha@gojek.com>
2021-05-03 01:18:51 +0530

    handle empty sessions

--- index.js
+++ index.js
@@ -28,24 +28,30 @@ async function callMain() {
     for(let obj of answers) {
         console.log("Appointments available for date ", obj.date);
         let sessions = obj.availability.sessions;
-        for (let session of sessions) {
-             let slotArray = session.slots;
-             let str = "slots available are :";
-             for(let i = 0; i < slotArray.length; i++) {
-                 str = str.concat(`\n${i + 1}. ${slotArray[i]}`);
-             }
-             session.slots = JSON.stringify(slotArray);
 
-             let {center_id, lat, long, session_id, ...formattedMsg} = session;
+        if (sessions.length == 0) {
+            message.sendMessageWhole(`No slots available for the date ${obj.date}`, config);
+        } else {
+            for (let session of sessions) {
+                let slotArray = session.slots;
+                let str = "slots available are :";
+                for(let i = 0; i < slotArray.length; i++) {
+                    str = str.concat(`\n${i + 1}. ${slotArray[i]}`);
+                }
+                session.slots = JSON.stringify(slotArray);
 
-             let answer = `For the date ${formattedMsg.date} the vaccine is available at Hospital ${formattedMsg.name} from ${formattedMsg.from} to ${formattedMsg.to}. Name of the vaccine available is ${formattedMsg.vaccine}. The capacity available is ${formattedMsg.available_capacity}. Fee charged for the vaccine is ${formattedMsg.fee}. And the ${str}`
-             message.sendMessageWhole(answer, config);
+                let {center_id, lat, long, session_id, ...formattedMsg} = session;
+
+                let answer = `For the date ${formattedMsg.date} the vaccine is available at Hospital ${formattedMsg.name} from ${formattedMsg.from} to ${formattedMsg.to}. Name of the vaccine available is ${formattedMsg.vaccine}. The capacity available is ${formattedMsg.available_capacity}. Fee charged for the vaccine is ${formattedMsg.fee}. And the ${str}`
+                message.sendMessageWhole(answer, config);
+            }
         }
+
         console.log(obj.availability);
     }
 }
 
-cron.schedule('*/5 * * * *', () => {
+cron.schedule('*/1 * * * *', () => {
     callMain().then(() => {
         console.log("Get Vaccinated !!!");
     });
